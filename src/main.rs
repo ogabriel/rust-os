e31671858d7be0ccd84de8ac8dc7c44a039d50d4
ogabriel <gabrieloliver8991@gmail.com>
2020-07-10 17:22:21 -0300

    add core to print to screen (with vga)

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,21 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 /// Use C calling convention for this function
 /// entrypoint
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize *2) = byte;
+            *vga_buffer.offset(i as isize *2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
