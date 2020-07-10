026a85dd6cfb676f8e93ca772027cf46b829e783
ogabriel <gabrieloliver8991@gmail.com>
2020-07-10 16:17:18 -0300

    add panic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 #![no_std]
 
 fn main() {}
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic (without std, we have to implement it)
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
