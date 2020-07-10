733b633042ec731274b3fe9a586064c6325a2efa
ogabriel <gabrieloliver8991@gmail.com>
2020-07-10 16:26:41 -0300

    Overwriting the Entry Point

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 #![no_std]
-
-fn main() {}
+#![no_main]
 
 use core::panic::PanicInfo;
 
 /// This function is called on panic (without std, we have to implement it)
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+
+/// Use C calling convention for this function
+/// entrypoint
+pub extern "C" fn _start() -> ! {
     loop {}
 }
